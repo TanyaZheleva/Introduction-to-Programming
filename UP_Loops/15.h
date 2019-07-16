39f718c3e59ba767b2bffa0eb2c36a3a61b7bc8c
TanyaZheleva <44448449+TanyaZheleva@users.noreply.github.com>
2019-07-16 15:34:08 +0300

    Loops

--- UP_Loops/15.h
+++ UP_Loops/15.h
@@ -1,1 +1,35 @@
 #pragma once
+#include <iostream>
+
+int main()
+{
+	int num;
+	std::cin >> num;
+	int myNum = 0;
+	bool guess = false;
+	bool bigger = false;
+	while (guess == false)
+	{
+		std::cout << "Is " << myNum << " your number?\n";
+		std::cin >> guess;
+		if (guess == true)
+		{
+			std::cout << "Yay!";
+			break;
+		}
+		else
+		{
+			std::cout << "Is your number bigger than " << myNum << "?\n";
+			std::cin >> bigger;
+			if (bigger)
+			{
+				myNum++;
+			}
+			else
+			{
+				myNum--;
+			}
+		}
+	}
+	return 0;
+}
