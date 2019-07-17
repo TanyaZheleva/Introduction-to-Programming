cf4383bfb2bf19162377821c1fc6ef8e221d3f9f
TanyaZheleva <44448449+TanyaZheleva@users.noreply.github.com>
2019-07-17 17:14:27 +0300

    array

---
+++ UP_Arrays/15.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <math.h>
+
+void allPossibleSubsets(int* _arr, int _size)
+{
+	int count = pow(2, _size);
+	for (int i = 0; i < count; i++)
+	{
+		for (int j = 0; j < _size; j++)
+		{
+			if ((i & (1 << j)) > 0)
+				std::cout << _arr[j] << " ";
+		}
+		std::cout << "yaa\n";
+	}
+}
+
+int main()
+{
+	int size;
+	std::cin >> size;
+	int* arr = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		std::cin >> arr[i];
+	}
+	allPossibleSubsets(arr,size);
+	return 0;
+}
--- UP_Arrays/Source.cpp
+++ UP_Arrays/Source.cpp
@@ -11,6 +11,6 @@
 //#include "11.h"
 //#include "12.h"
 //#include "13.h"
-#include "14.h"
-//#include "15.h"
+//#include "14.h"
+#include "15.h"
 //#include "16.h"
