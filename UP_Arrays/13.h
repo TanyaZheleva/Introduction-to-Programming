cc4a468e91ebd467b3eb2ac831651485ab7bb935
TanyaZheleva <44448449+TanyaZheleva@users.noreply.github.com>
2019-07-17 13:23:51 +0300

    arrays

--- UP_Arrays/13.h
+++ UP_Arrays/13.h
@@ -1,6 +1,38 @@
 #pragma once
 #include <iostream>
 
+void longest(int* _arr, int _size)
+{
+	int count = 0;
+	int lastIndex = 0;
+	int highestCount = 0;
+	int hcLastIndex = 0;
+	for (int i = 0; i < _size - 1; i++)
+	{
+		if (_arr[i] < _arr[i + 1])
+		{
+			lastIndex = i;
+			count++;
+		}
+		else
+		{
+			if (highestCount < count)
+			{
+				highestCount = count;
+				hcLastIndex = lastIndex;
+			}
+			count = 0;
+			lastIndex = 0;
+		}
+	}
+
+	int firstIndex = hcLastIndex - highestCount + 1;
+	for (int i = firstIndex; i <= hcLastIndex+1; i++)
+	{
+		std::cout << _arr[i];
+	}
+}
+
 int main()
 {
 	int size;
@@ -10,6 +42,6 @@ int main()
 	{
 		std::cin >> arr[i];
 	}
-
+	longest(arr, size);
 	return 0;
 }
