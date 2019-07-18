90fb2f15b148958f7282cf9a10d493e79863b894
TanyaZheleva <44448449+TanyaZheleva@users.noreply.github.com>
2019-07-18 16:36:14 +0300

    matrices

--- UP_Matrices/8.h
+++ UP_Matrices/8.h
@@ -13,7 +13,7 @@ int main()
 		matrix[i] = new int[size];
 	}
 	//a
-/*	int n = 1;
+    /*int n = 1;
 	for (int i = 0; i < size; i++)
 	{
 		for (int j = 0; j < size; j++)
@@ -24,13 +24,13 @@ int main()
 	}*/
 
 	//b
-	int num = 1;
+	/*int num = 1;
 	int index = 0;
 	for (int i = 0; i < size; i++)
 	{
 		for (int j = 0; j < size; j++)
 		{
-			
+
 			matrix[index][i] = num;
 			num++;
 			if (i%2==0)
@@ -51,6 +51,47 @@ int main()
 			index++;
 		}
 	}
+	*/
+
+	//c
+	/*int num = 1;
+	int p = 0;
+	int q = 0;
+	for (int i = 1; i <= size; i++)
+	{
+		q = 0;
+		p = size - i;
+		matrix[p][q] = num;
+		num++;
+
+		while (p < size - 1)
+		{
+			p++;
+			q++;
+			matrix[p][q] = num;
+			num++;
+		}
+	}
+
+	int s = 0;
+	int t = 0;
+	for (int j = 1; j < size; j++)
+	{
+		s = 0;
+		t = j;
+		matrix[s][t] = num;
+		num++;
+		while (t < size-1)
+		{
+			t++;
+			s++;
+			matrix[s][t] = num;
+			num++;
+		}
+	}*/
+
+	//d
+
 
 	for (int i = 0; i < size; i++)
 	{
