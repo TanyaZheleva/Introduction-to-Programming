fab19cf0529156c16daa87403a4fe759d8ab0e4a
TanyaZheleva <44448449+TanyaZheleva@users.noreply.github.com>
2019-07-19 11:19:31 +0300

    hw_summer

---
+++ hw_ex1/2.cpp
@@ -0,0 +1,21 @@
+/**
+*
+* Solution to homework task 09
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* 2018/2019 academic year
+*
+* @author Tanya Zheleva
+* @idnumber 62288
+* @task 2
+* @compiler  VC
+*
+*/
+
+#include<iostream>
+
+int main()
+{
+
+	return 0;
+}
