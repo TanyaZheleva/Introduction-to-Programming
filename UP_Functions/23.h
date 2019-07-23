58a480b4e9a328154a4998e325c96c536a3ce8a9
TanyaZheleva <44448449+TanyaZheleva@users.noreply.github.com>
2019-07-23 14:19:05 +0300

    Functions

--- UP_Functions/23.h
+++ UP_Functions/23.h
@@ -1,8 +1,37 @@
 #pragma once
 #include <iostream>
 
+int tribonacci(int num)
+{
+	int pppt = 0;
+	int ppt = 0;
+	int pt = 1;
+	int t = 1;
+	if (num > 4)
+	{
+		for (int i = 3; i < num; i++)
+		{
+			t = pt + ppt+pppt;
+			pppt = ppt;
+			ppt = pt;
+			pt = t;
+		}
+	}
+	if (num < 3)
+	{
+		return 0;
+	}
+	if(num>2&&num<5)
+	{
+		return 1;
+	}
+	return t;
+}
+
 int main()
 {
-
+	int num;
+	std::cin >> num;
+	std::cout << tribonacci(num);
 	return 0;
 }
