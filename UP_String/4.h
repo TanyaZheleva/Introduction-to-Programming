6e0e467cbfe71305b9b084aa81880b84921cc2d6
TanyaZheleva <44448449+TanyaZheleva@users.noreply.github.com>
2019-07-24 17:40:30 +0300

    string

--- UP_String/4.h
+++ UP_String/4.h
@@ -1,1 +1,24 @@
 #pragma once
+#include <iostream>
+
+bool palindrome(const char* str)
+{
+	int length = strlen(str);
+	for (int i = 0; i < length / 2; i++)
+	{
+		if (str[i] != str[length - i-1])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	char* str = new char[100];
+	str[0] = '\0';
+	std::cin >> str;
+	std::cout << palindrome(str);
+	return 0;
+}
