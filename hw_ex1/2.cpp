42044f66b20df334a77d221386072d82b472bc3f
TanyaZheleva <44448449+TanyaZheleva@users.noreply.github.com>
2019-07-26 16:07:25 +0300

    hw

--- hw_ex1/2.cpp
+++ hw_ex1/2.cpp
@@ -1,66 +1,95 @@
 
-//#include<iostream>
-//
-//int main()
-//{
-//	/*unsigned int day;
-//	unsigned int month;
-//	unsigned int year;
-//	
-//	std::cout << "Enter date!\n";
-//	std::cin >> day;
-//	std::cout << "Enter month!\n";
-//	std::cin >> month;
-//	std::cout << "Enter year!\n";
-//	std::cin >> year;
-//
-//	if (month > 12 || month < 1)
-//	{
-//		std::cout << "Invalid month!\n";
-//		std::cin >> month;
-//	}
-//
-//	if (year < 1 )
-//	{
-//		std::cout << "Invalid year!\n";
-//		std::cin >> year;
-//	}
-//
-//	if(day<1)
-//	{
-//		std::cout << "Invalid date!\n";
-//		std::cin >> day;
-//	}
-//
-//	if (((month<8&&month%2!=0)||(month>7&&month%2==0))&&day>31)
-//	{
-//		std::cout << "Invalid date!\n";
-//		std::cin >> day;
-//	}
-//
-//	if (((month < 8 && month % 2 == 0) || (month > 7 && month % 2 != 0)) && day > 30)
-//	{
-//		std::cout << "Invalid date!\n";
-//		std::cin >> day;
-//	}
-//	if ((year % 100) / 4 == 0 && month == 2 && day > 29)
-//	{
-//		std::cout << "Invalid date!\n";
-//		std::cin >> day;
-//	}
-//	*/
-//	char date[11];
-//	for (int i = 0; i < 10; i++)
-//	{
-//		std::cin >> date[i];
-//	}
-//	date[10] = '\0';
-//	
-//
-//	
-//	for (int i = 0; i < 10; i++)
-//	{
-//		std::cout<< date[i];
-//	}
-//	return 0;
-//}
+
+#include<iostream>
+
+int main()
+{
+	char date[11];
+	int day;
+	int month;
+	int year;
+	int temp = 0;
+	int step = 3;
+	bool correctDate = false;
+	while (correctDate == false)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			std::cin >> date[i];
+		}
+		date[10] = '\0';
+
+		day = 0;
+		month = 0;
+		year = 0;
+		temp = 0;
+		step = 3;
+		for (int i = 0; i < 10; i++)
+		{
+			if (i < 2)
+			{
+				temp = date[i] - 48;
+				day += temp * pow(10, 1 - i);
+			}
+			if (i > 2 && i < 5)
+			{
+				temp = date[i] - 48;
+				month += temp * pow(10, i - pow(2, i - 2));
+			}
+			if (i > 5)
+			{
+				temp = date[i] - 48;
+				year += temp * pow(10, step);
+				step--;
+			}
+		}
+		
+		if (year > 0)
+		{
+			if (month > 0 && month < 12)
+			{
+				if ((year % 100) / 4 == 0 && month == 2 && day <= 29)
+				{
+					correctDate = true;
+				}
+				if ((year % 100) / 4 != 0 && month == 2 && day <= 28)
+				{
+					correctDate = true;
+				}
+				if (((month < 8 && month % 2 != 0) || (month > 7 && month % 2 == 0)) && month != 2 && day <= 31)
+				{
+					correctDate = true;
+				}
+
+				if (((month < 8 && month % 2 == 0) || (month > 7 && month % 2 != 0)) && month != 2 && day <= 30)
+				{
+					correctDate = true;
+				}
+			}
+		}
+		if (correctDate == false)
+		{
+			std::cout << "Wrong date! Please enter again!\n";
+		}
+	}
+
+	int daysPassed = 0;
+	daysPassed = (year - 1) * 365 + day - 1 + (month - 1) * 30;
+	for (int i = 0; i < year; i++)
+	{
+		if ((i % 100) / 4 == 0)
+		{
+			daysPassed++;
+		}
+	}
+	for (int i = 0; i < month; i++)
+	{
+		if (((month < 8 && month % 2 != 0) || (month > 7 && month % 2 == 0)))
+		{
+			daysPassed++;
+		}
+	}
+
+	std::cout << daysPassed << " days since 01.01.0001\n";
+	return 0;
+}
