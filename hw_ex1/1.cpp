8e06b6ec611fd52fbccdf5fe1bda0e24e2cf72e3
TanyaZheleva <44448449+TanyaZheleva@users.noreply.github.com>
2019-07-29 17:08:59 +0300

    hw

--- hw_ex1/1.cpp
+++ hw_ex1/1.cpp
@@ -1,63 +1,54 @@
-///**
-//*
-//* Solution to homework task 09
-//* Introduction to programming course
-//* Faculty of Mathematics and Informatics of Sofia University
-//* 2018/2019 academic year
-//*
-//* @author Tanya Zheleva
-//* @idnumber 62288
-//* @task 1
-//* @compiler  VC
-//*
-//*/
-//
-//#include <iostream>
-//
-//int getLength(int number)
-//{
-//	int size = 0;
-//	while (number != 0)
-//	{
-//		number /= 10;
-//		size++;
-//	}
-//	return size;
-//}
-//
-//int* getNumbers(int num)
-//{
-//	int size = getLength(num);
-//	int* numbers = new int[size];
-//	int divisor = 1;
-//	for (int i = 0; i < size; i++)
-//	{
-//		divisor = pow(10, size - i - 1);
-//		numbers[i] = num / divisor;
-//		num = num % divisor;
-//	}
-//	return numbers;
-//}
-//
-//
-//int main()
-//{
-//	int num=0;
-//	while (num<65000 && num>-65000)
-//	{
-//		std::cin >> num;
-//		if (num<65000 && num>-65000)
-//		{
-//			int* arr = getNumbers(num);
-//			int length = getLength(num);
-//
-//			for (int i = 0; i < length; i++)
-//			{
-//				std::cout << arr[i] << " ";
-//			}
-//			std::cout << '\n';
-//			delete[] arr;
-//		}
-//	}
-//	return 0;
-//}
+#include <iostream>
+
+int getLength(int number)
+{
+	int size = 0;
+	while (number != 0)
+	{
+		number /= 10;
+		size++;
+	}
+	return size;
+}
+
+int* getNumbers(int num)
+{
+	int size = getLength(num);
+	int* numbers = new int[size];
+	int divisor = 1;
+	for (int i = 0; i < size; i++)
+	{
+		divisor = pow(10, size - i - 1);
+		numbers[i] = num / divisor;
+		num = num % divisor;
+	}
+	return numbers;
+}
+
+
+int main()
+{
+	int num=0;
+	while (num>-65000 && num < 65000)
+	{
+		std::cin >> num;
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+		if (num > -65000 && num < 65000)
+		{
+			int* arr = getNumbers(num);
+			int length = getLength(num);
+
+			for (int i = 0; i < length; i++)
+			{
+				std::cout << arr[i] << " ";
+			}
+			std::cout << '\n';
+			delete[] arr;
+		}
+	}
+	return 0;
+}
