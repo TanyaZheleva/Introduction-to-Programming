e6883a9eeb763fbce2256bd758e02b0d93a281a4
TanyaZheleva <44448449+TanyaZheleva@users.noreply.github.com>
2019-07-30 17:00:05 +0300

    hw

--- hw/2.cpp
+++ hw/2.cpp
@@ -1,3 +1,16 @@
+/**
+*
+* Solution to homework task 09
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* 2018/2019 academic year
+*
+* @author Tanya Zheleva
+* @idnumber 62288
+* @task 2
+* @compiler  VC
+*
+*/
 
 #include<iostream>
 
--- hw/3.cpp
+++ hw/3.cpp
@@ -1,3 +1,17 @@
+/**
+*
+* Solution to homework task 09
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* 2018/2019 academic year
+*
+* @author Tanya Zheleva
+* @idnumber 62288
+* @task 3
+* @compiler  VC
+*
+*/
+
 #include<iostream>
 
 int main()
