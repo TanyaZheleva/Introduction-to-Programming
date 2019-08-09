f1bf4ef561cc902b0d2f916bd334bc07958102ea
TanyaZheleva <44448449+TanyaZheleva@users.noreply.github.com>
2019-08-09 16:30:30 +0300

    zigzag
    print a string in a zigzag pattern

--- Extra Exercises/22.h
+++ Extra Exercises/22.h
@@ -1,32 +1,75 @@
 #pragma once
 #include <iostream>
 
-void zigzag(char* str, int size,int lines)
+void zigzag(char* str, int size, int lines)
 {
 	int i = 0;//index of line
-	int j = 0;//index in string
+	int j = 1;//index in string
 	//int frontSpaces = 0;
 	int intervals = 2 * lines - 3;
-	while (j < size)
+	int temp;
+	//start first line
+	for (int t = 0; t < size; t += 2 * lines - 2)
 	{
-		if (i < lines)
+
+		std::cout << str[t];
+		for (int p = 0; p < intervals; p++)
 		{
-			for ( int q = 0; q < i; q++)
+			std::cout << " ";
+		}
+	}
+	std::cout << "\n";
+	//end first line
+	i++;
+	intervals -= 2;
+	temp = j;
+	int repeats = 0;
+	while (i < lines)
+	{
+		temp = j;
+		repeats = 0;
+		while (temp + 2 * i < size)
+		{
+			for (int q = 0; q < i; q++)
 			{
 				std::cout << ' ';
 			}
-			std::cout << str[j];
+
+			if (repeats > 0)
+			{
+				temp += 2 * i;
+			}
+
+			std::cout << str[temp];
+
 			for (int p = 0; p < intervals; p++)
 			{
 				std::cout << " ";
 			}
-			i++;
-			j++;
-		}
-		if (i >= lines)
-		{
+			if (intervals != 0)
+			{
+				temp += intervals + 1;
+				if (temp < size)
+				{
+					std::cout << str[temp];
+				}
+			}
 
+			for (int p = 1; p < i; p++)
+			{
+				std::cout << ' ';
+			}
+
+			repeats++;
 		}
+		i++;
+		j++;
+		intervals -= 2;
+		if (intervals < 0)
+		{
+			intervals = 0;
+		}
+		std::cout << '\n';
 	}
 }
 
@@ -36,9 +79,9 @@ int main()
 	std::cin >> size;
 	char* sentance = new char[size + 1];
 	std::cin.get();
-	std::cin.getline(sentance, size+1);
+	std::cin.getline(sentance, size + 1);
 	int lines;
 	std::cin >> lines;
-	zigzag(sentance, size,lines);
+	zigzag(sentance, size, lines);
 	return 0;
 }
